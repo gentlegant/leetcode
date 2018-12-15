6dfc65b41ba50a54e745d29bdf40afb4d6af940e
jxk <740264606@qq.com>
2018-12-16 02:18:57 +0800

    leetcode 23男人的提交，超过百分之96%

--- leetcode23.cpp
+++ leetcode23.cpp
@@ -30,21 +30,22 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-class Solution {
 
+class Solution {
     //losertrees记录败者下标
     //winner记录胜者下标
     //number记录初始数组
     vector<int> losertree;
     vector<int> winner;
     vector<int> numbers;
-    int getN(vector<ListNode*>& lists,int n){
-        if(!lists[n])
-            return INT32_MAX;
+    void getN(vector<ListNode*>& lists,int n){
+        if(!lists[n]){
+            numbers[n]=INT32_MAX;
+            return;
+        }    
         int rem=lists[n]->val;
         lists[n]=lists[n]->next;
         numbers[n]=rem;
-        return rem;
     }
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
@@ -57,7 +58,7 @@ public:
         for(int i=0;i<k;i++){
             getN(lists,i);
             losertree[k-1+i]=i;
-            winner[k-i+1]=i;
+            winner[k-1+i]=i;
         }
         for(int i=k-2;i>=0;i--){
             int left=winner[2*i+1];
@@ -74,42 +75,39 @@ public:
         //至此败者树已建立完毕，winner 作为临时变量不再使用
         //win表示胜者得下标
         int win=winner[0];
- 
         while(numbers[win]!=INT32_MAX) {
             temp->next=new ListNode(numbers[win]);
             temp=temp->next;
             getN(lists,win);
-     
             //从win往上遍历，tmp是win对应的值，win是下标
-            for(int up=win;up!=0 ;up=(up-1)/2){
+            for(int up=win+k-1;;up=(up-1)/2){
                 //父节点记录了败者
                 //tmp是新来的数，win是其下标
-                if(numbers[losertree[up]]<numbers[win])
-                                   
+                if(numbers[losertree[up]]<numbers[win])                                   
                     swap(win,losertree[up]);
+                if(up==0)
+                    break;
             }
-
-
-
-
-
         }
-
         return head->next;
     }
 };
 int main(int argc, char const *argv[])
 {
     /* code */
-    ListNode *tmp=new ListNode(5);
-    tmp->next=new ListNode(7);
+    ListNode *tmp=new ListNode(2);
+    tmp->next=new ListNode(5);
+
     ListNode *tmp1=new ListNode(3);
     tmp1->next=new ListNode(6);
-    vector<ListNode*> vc={tmp,tmp1};
+    ListNode *tmp2=new ListNode(4);
+    tmp2->next=new ListNode(6);
+    ListNode *tmp3=new ListNode(6);
+    vector<ListNode*> vc={tmp1,tmp2,tmp,tmp3};
     Solution a;
-    cout<<1;
+
     auto b=a.mergeKLists(vc);
-    cout<<3;
+
     while(b){
 
         cout<<b->val;
